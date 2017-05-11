c5541844d74369fdf591b784324bd313ac3b1e19
Jesús Muriel <jmuriel@itbid.org>
2017-05-11 13:09:26 +0200

    First Commit. Square Grid Functionality with three columns

---
+++ js/grid-square.js
@@ -0,0 +1,26 @@
+var squareHeight = null;
+
+$(document).ready(function() {
+    $('.square').each(function() {
+        var square = $(this);
+
+        if (squareHeight != null) {
+            square.css('height', squareHeight);
+            return true;
+        }
+
+        var divisor = 1;
+        if (square.hasClass('x2')) {
+            divisor = 2;
+        }
+        if (square.hasClass('x3')) {
+            divisor = 3;
+        }
+
+        var width = square.css('width').replace('px','');
+
+        squareHeight = width/divisor;
+
+        square.css('height', width/divisor);
+    });
+});
